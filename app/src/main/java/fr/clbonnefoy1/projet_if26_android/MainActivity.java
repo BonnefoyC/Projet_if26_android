0f6c590556a82e38f3b827ed7cb1a44d8269b7c1
BonnefoyC <clementbonnefoy1@gmail.com>
2017-11-17 12:13:25 +0100

    Context

--- app/src/main/java/fr/clbonnefoy1/projet_if26_android/MainActivity.java
+++ app/src/main/java/fr/clbonnefoy1/projet_if26_android/MainActivity.java
@@ -1,13 +1,19 @@
 package fr.clbonnefoy1.projet_if26_android;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
     }
 }
