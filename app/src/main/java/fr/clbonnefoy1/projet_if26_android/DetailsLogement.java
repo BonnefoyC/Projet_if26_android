abe5842ecf42d6d770b0a9ce838cde50c8a4d732
wiemjelladif26 <wiem.jellad@utt.fr>
2017-12-19 17:37:56 +0100

    Interface details.

--- app/src/main/java/fr/clbonnefoy1/projet_if26_android/DetailsLogement.java
+++ app/src/main/java/fr/clbonnefoy1/projet_if26_android/DetailsLogement.java
@@ -23,11 +23,26 @@ public class DetailsLogement extends AppCompatActivity {
 
         Log.i("DetailsLogement", intent.getStringExtra("label"));
 
+        String label= intent.getStringExtra("label");
+
+        ModulePersistance mp=new ModulePersistance(this);
+
+        Logement l=mp.getLogement(label);
+
         tv_label = (TextView)findViewById(R.id.tv_label);
-        tv_prix =  (TextView)findViewById(R.id.tv_prix);
+        tv_label.setText(l.getLabel());
+        tv_prix = (TextView)findViewById(R.id.tv_prix);
+        tv_prix.setText(l.getPrix());
+
         tv_nbre_pieces = (TextView)findViewById(R.id.tv_nbre_pieces);
+        tv_nbre_pieces.setText(l.getNb_pieces());
+
         tv_description = (TextView)findViewById(R.id.tv_description);
+        tv_description.setText(l.getDescription());
+
         tv_ville = (TextView)findViewById(R.id.tv_ville);
+        tv_ville.setText(l.getVille());
+
 
     }
 }
