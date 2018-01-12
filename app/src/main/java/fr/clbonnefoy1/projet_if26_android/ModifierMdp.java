888fa4f30be27b7d3551341c2b66f8d21f302a8e
wiemjelladif26 <wiem.jellad@utt.fr>
2018-01-12 09:16:51 +0100

    Modification mot de passe

--- app/src/main/java/fr/clbonnefoy1/projet_if26_android/ModifierMdp.java
+++ app/src/main/java/fr/clbonnefoy1/projet_if26_android/ModifierMdp.java
@@ -1,12 +1,16 @@
 package fr.clbonnefoy1.projet_if26_android;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import java.util.Random;
 
 public class ModifierMdp extends AppCompatActivity {
 
@@ -22,10 +26,14 @@ public class ModifierMdp extends AppCompatActivity {
     private Button bt_num_temp;
     private Button bt_mail;
 
+    private int code;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modifier_mdp);
+
+        mContext = this;
 
         ll_mdp = (LinearLayout)findViewById(R.id.ll_mdp);
         et_ancien_mdp = (EditText)findViewById(R.id.et_ancien_mdp);
@@ -41,12 +49,28 @@ public class ModifierMdp extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //descativate ll_mpd components
-                et_ancien_mdp.setEnabled(false);
-                et_nouveau_mdp.setEnabled(false);
-                bt_mdp.setClickable(false);
+                String ancien_mdp = et_ancien_mdp.getText().toString();
+                String nouveau_mdp = et_nouveau_mdp.getText().toString();
 
-                ll_num_temp.setVisibility(View.VISIBLE);
+
+                if(!ancien_mdp.equals(nouveau_mdp)) {
+                    //descativate ll_mpd components
+                    et_ancien_mdp.setEnabled(false);
+                    et_nouveau_mdp.setEnabled(false);
+                    bt_mdp.setClickable(false);
+
+                    ll_num_temp.setVisibility(View.VISIBLE);
+
+                    sendSms();
+
+                } else {
+                    LinearLayout mRootView = (LinearLayout) findViewById(R.id.LinearLayout);
+                    Snackbar.make(mRootView,"Les deux mots de passe sont identiques", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+
+
 
             }
         });
@@ -54,6 +78,17 @@ public class ModifierMdp extends AppCompatActivity {
         bt_num_temp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String num_temp = et_num_temp.getText().toString();
+                if ( code == Integer.parseInt(num_temp)){
+                    LinearLayout mRootView = (LinearLayout) findViewById(R.id.LinearLayout);
+                    Snackbar.make(mRootView,"Le mot de passe est modifié", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                } else {
+                    LinearLayout mRootView = (LinearLayout) findViewById(R.id.LinearLayout);
+                    Snackbar.make(mRootView,"Le code n'est pas bon", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
 
             }
         });
@@ -61,13 +96,21 @@ public class ModifierMdp extends AppCompatActivity {
         bt_mail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                sendSms();
+                LinearLayout mRootView = (LinearLayout) findViewById(R.id.LinearLayout);
+                Snackbar.make(mRootView,"SMS envoyé", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
 
-    private void sendEmail(){
+    private void sendSms(){
         //TODO
+        Random r = new Random();
+        code = r.nextInt(8999) + 1000;
+        Log.i("code", "" + code);
+
+        //SMS
     }
 
 }
